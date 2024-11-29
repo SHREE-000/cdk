563590541f91efc3f433a11e13f8aefec9548a7f
Shreehari C R <shreehari000@gamil.com>
2024-11-29 07:31:59 +0530

    Added apiStack

--- space-finder/src/infra/stacks/ApiStack.ts
+++ space-finder/src/infra/stacks/ApiStack.ts
@@ -1,10 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { AuthorizationType, CognitoUserPoolsAuthorizer, LambdaIntegration, MethodOptions, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { IUserPool } from 'aws-cdk-lib/aws-cognito';
+import { AuthorizationToken } from 'aws-cdk-lib/aws-ecr';
 
 interface ApiStackProps extends StackProps {
     // helloLambdaIntegration: LambdaIntegration
-    spacesLambdaIntegration: LambdaIntegration
+    spacesLambdaIntegration: LambdaIntegration,
+    userPool: IUserPool;
 }
 
 export class ApiStack extends Stack {
@@ -13,8 +16,23 @@ export class ApiStack extends Stack {
 
         const api = new RestApi(this, "SpaceApi");
         const spacesResource = api.root.addResource("spaces");
+
+        const authorizer = new CognitoUserPoolsAuthorizer(this, 'SpacesApiAuthorizer', {
+            cognitoUserPools:[props.userPool],
+            identitySource: 'method.request.header.Authorization'
+        });
+        authorizer._attachToApi(api);
+        const optionsWithAuth: MethodOptions = {
+            authorizationType: AuthorizationType.COGNITO,
+            authorizer: {
+                authorizerId: authorizer.authorizerId
+            }
+        }
+
         // spacesResource.addMethod("GET");
-        spacesResource.addMethod('GET', props.spacesLambdaIntegration);
-        spacesResource.addMethod('POST', props.spacesLambdaIntegration);
+        // spacesResource.addMethod('GET', props.spacesLambdaIntegration);
+        // spacesResource.addMethod('POST', props.spacesLambdaIntegration);
+        spacesResource.addMethod('GET', props.spacesLambdaIntegration, optionsWithAuth);
+        spacesResource.addMethod('POST', props.spacesLambdaIntegration,optionsWithAuth);
     }
 }
