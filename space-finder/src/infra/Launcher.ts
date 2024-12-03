9dd80206f264fb6e6388d3e739fe1b3a7350dd84
Shreehari C R <shreehari000@gamil.com>
2024-12-03 15:58:37 +0530

    added launcher

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -2,13 +2,15 @@ import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
 import { ApiStack } from "./stacks/ApiStack";
+import { AuthStack } from "./stacks/AuthStack";
 
 const app = new App();
 const dataStack = new DataStack(app, "DataStack");
 const lambdaStack = new LambdaStack(app, "LambdaStack", {
     spaceTable: dataStack.spaceTable
 });
+const authStack = new AuthStack(app, 'AuthStack');
 new ApiStack(app, 'ApiStack', {
-    // helloLambdaIntegration: lambdaStack.helloLambdaIntegration
-    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration
+    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
+    userPool: authStack.userPool
 })
